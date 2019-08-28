8a97ead50200bf095a7d5f4ac93abdccfa04842c
Shimon <shimsh16@gmail.com>
2019-08-28 18:48:59 +0300

    sdfdsf

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gridy">
+        <div >ffff</div>
+        <div > ffff efe ef ef ef e er er er </div>
+        <div >ffff</div>
+        <div >ffff efe ef ef ef e er er er </div>
+        <div >ffff wwwer ef ef ef e er er er </div>
+        <div className="nested">
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+        </div>  
+        <div className="nested2">
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+          <div>text</div>
+        </div>     
+       </div>
+       <div className="grid2">
+         <div className="sideManu">1</div>
+         <div>2</div>
+         <div>1</div>
+         <div>1</div>
+         <div>1</div>
+         <div>1</div>
+         <div>1</div>
+         <div>1</div>
+         <div>1</div>
+       </div>
+    </div>
+  );
+}
+
+export default App;
+{/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edita <code>src/App.js</code> and save to reload!
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header> */}
